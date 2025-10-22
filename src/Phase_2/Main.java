878208fa1a88f547bf99718da0df22b552041210
Gauravsakure <gau143sak@gmail.com>
2025-10-22 12:16:53 +0530

    4th

--- src/Phase_2/Main.java
+++ src/Phase_2/Main.java
@@ -8,12 +8,12 @@ public class Main {
         //printodd();
         //printeven();
         //down();
-        table(8);
-
-
-
-
-
+        //table(8);
+        //naturaln(5);
+        //evensum(100);
+        //oddsum(100);
+        //factorial(6);
+        //digitproduct(0);
     }
 
 
@@ -52,5 +52,54 @@ public class Main {
             System.out.println(n + " x " + i + " = " + (n * i));
         }
         System.out.println();
+    }
+
+    static void naturaln(int n){
+        System.out.println("Natural no sum = "+ n*(n+1)/2);
+    }
+
+    static void evensum(int n){
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                sum += i;
+
+            }
+        }
+        System.out.println("even sum = "+sum);
+    }
+
+    static void oddsum(int n){
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                sum += i;
+
+            }
+        }
+        System.out.println("odd sum = "+sum);
+    }
+
+    static void factorial(int n){
+        long product = 1;
+        for(int i=n;i>=1;i--){
+            product = product *i;
+        }
+        System.out.println("factorial = "+product);
+    }
+
+    static void digitproduct(int n){
+        if (n == 0) {
+            System.out.println("digitproduct = 0");
+            return;
+        }
+        int temp = Math.abs(n);
+        long product = 1;
+        while(temp>0){
+            int last = temp % 10;
+            product = product*last;
+            temp /= 10;
+        }
+        System.out.println("digitproduct = "+product);
     }
 }
