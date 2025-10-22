b4852bb7480d21f58bd9d7cabd6373e7e37d2219
Gauravsakure <gau143sak@gmail.com>
2025-10-22 11:54:28 +0530

    3rd

--- src/Phase_2/Main.java
+++ src/Phase_2/Main.java
@@ -6,7 +6,14 @@ public class Main {
 
         //printnos();
         //printodd();
-        printeven();
+        //printeven();
+        //down();
+        table(8);
+
+
+
+
+
     }
 
 
@@ -28,6 +35,21 @@ public class Main {
     static void printeven(){
         for(int i=2;i<=100;i+=2){
             System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+
+
+    static void down(){
+        for(int i=10;i>=1;i--){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+
+    static void table(int n){
+        for(int i=1;i<=10;i++){
+            System.out.println(n + " x " + i + " = " + (n * i));
         }
         System.out.println();
     }
