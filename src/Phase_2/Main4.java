cc696a4edad034d2c5a3d1ce48347775ac09c884
Gauravsakure <gau143sak@gmail.com>
2025-11-05 13:09:08 +0530

    1

--- src/Phase_2/Main4.java
+++ src/Phase_2/Main4.java
@@ -2,7 +2,27 @@ package Phase_2;
 
 public class Main4 {
 
+
+    public static void simple(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    static void one(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
+        //simple(4);
 
     }
 }
