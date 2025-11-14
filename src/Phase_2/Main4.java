8d06b85097493e262d20b3081657345c0f6eadb1
Gauravsakure <gau143sak@gmail.com>
2025-11-14 16:59:35 +0530

    28

--- src/Phase_2/Main4.java
+++ src/Phase_2/Main4.java
@@ -236,6 +236,24 @@ public class Main4 {
         }
     }
 
+    static void nineteen(int n){
+        //upper
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //lower
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         //simple(4);
         //one(5);
@@ -255,7 +273,7 @@ public class Main4 {
         //fifteen(26);
         //sixteen(6);
         //eighteen(5);
-
+        nineteen(5);
 
 
     }
